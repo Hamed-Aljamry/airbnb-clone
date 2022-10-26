520e319b951675e5b5ddbac882cb2708e33205f4
hamed aljamry <hamed970@hotmail.com>
2022-10-26 23:51:23 +0100

    added transitions from el-transition doc to headerhtml

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -2,9 +2,8 @@ import { Controller } from '@hotwired/stimulus'
 import {enter, leave, toggle} from 'el-transition'
 
 export default class extends Controller {
+  static targets = ['openUserMenu'];
   connect() {
-    console.log('enter: ', enter);
-    console.log('leave: ', leave);
-    console.log('toggle: ', toggle);
+
   }
 }
